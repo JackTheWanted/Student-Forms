ed0f56a845bb02cd85cb9643ce702ec7878985ee
Jack Carlson <CarlJack215@GoTVDSB.ca>
2020-10-14 10:25:24 -0400

    Add project files.

---
+++ MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Forms
+{
+    public partial class StudentForms : Form
+    {
+        public StudentForms()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentForms_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
